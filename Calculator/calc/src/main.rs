52b93e8249e811387527b5135778e9c0c0962a1e
joseinweb <joseinweb@gmail.com>
2025-05-07 09:52:47 -0400

    folder structure and project name change

--- Calculator/calc/src/main.rs
+++ Calculator/calc/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
     println!("Hello, I am a calculator!");
     let x = 10;
     let y=2;
-    let sum = adder::add(x, y);
-    let diff = differ::diff(x, y);
+    let sum = addition::add(x, y);
+    let diff = substraction::diff(x, y);
     let div = divide::divide(x, y);
     let product = product::mul(x, y);
     println!("The sum of {} and {} is {}", x, y, sum);
